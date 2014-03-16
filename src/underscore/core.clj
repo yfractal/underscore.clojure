19af029d872ed212ce1891e5c09b74e10075f60b
yfractal <mike@beansmile.com>
2014-03-16 12:05:37 +0800

    get num-of-arguments of a function

---
+++ src/underscore/core.clj
@@ -0,0 +1,21 @@
+(ns underscore.core)
+
+(defn reverse-join [pattern string]
+  (clojure.string/join string pattern))
+
+(defn- get-full-name [f]
+  (-> f str (clojure.string/split #"[\$@]") butlast (reverse-join "/")))
+
+(defn get-fun [full-name]
+  (->> full-name (str "#'") load-string))
+
+(defn arglists [fun]
+  (let [meta* (meta  fun)]
+    (:arglists meta*)))
+
+(defn count-argument [arglists]
+  (-> arglists first count))
+
+(defn num-of-arguments [f]
+  (-> f get-full-name get-fun arglists count-argument))
+
---
+++ test/underscore/core_test.clj
@@ -0,0 +1,23 @@
+(ns underscore.core-test
+  (:require [clojure.test :refer :all]
+            [midje.sweet :refer :all]
+            [underscore.core :refer :all]))
+
+;(defn f [])
+;(defn f1 [a])
+;(defn f2 [a b])
+;(deftest get-fun-full-name
+;  (is (#'underscore.core/get-full-name f)  "underscore.core_test/f"))
+
+;;in let? #() fn??
+;(fact "get acture fun"
+      ;(#'underscore.core/acture-fun "underscore.core_test/f") =>
+      ;)
+
+;(future-fact "know how many arguments the a function needs"
+      ;(num-of-arguments f1) => 2
+      ;(num-of-arguments f2) => 2)
+
+;(deftest get-num-of-arguments
+ ; (is (num-of-arguments f1) 1)
+  ;(is (num-of-arguments f2) 2))
